988e11a214d0af3a19c194fceb66d96204d272a4
iamseeley <seeleypersonal@gmail.com>
2024-03-12 17:01:27 -0400

    adding apple icon png

--- app/api/faviconpng/route.tsx
+++ app/api/faviconpng/route.tsx
@@ -3,6 +3,11 @@ import { NextRequest } from 'next/server';
 
 export const runtime = "edge"
 
+export const size = {
+  width: 32,
+  height: 32,
+}
+
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const emoji = searchParams.get('emoji');
@@ -12,36 +17,28 @@ export async function GET(req: NextRequest) {
     return new Response('Invalid request', { status: 400 });
   }
 
-  const isMobile = Boolean(
-    userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
-  );
-
+ 
  return new ImageResponse(
     (
       <div
         style={{
-          backgroundColor: 'white',
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontFamily: isMobile ? 'Apple Color Emoji' : 'sans-serif', // <-- Add this line
+            fontSize: 24,
+            background: 'black',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
         }}
       >
-        <div
-          style={{
-            fontSize: isMobile ? '96px' : '48px',
-          }}
-        >
           {emoji}
-        </div>
       </div>
     ),
     {
-      width: isMobile ? 128 : 64,
-      height: isMobile ? 128 : 64,
-      emoji: 'twemoji', // <-- Update this line
+      ...size
     }
   );
 }
+
+ 
