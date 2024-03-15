d1cf68d2a68f0550c8f76152643886b69e89eb1b
iamseeley <seeleypersonal@gmail.com>
2024-03-15 16:41:47 -0400

    trying to get png favicon to work on ios and safari

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return  (
         <footer className="border-t">
             <div className='max-w-3xl mx-auto px-4 py-2 flex flex-row gap-4'>
-                <p className=""><Link className="opacity-60 hover:opacity-100" target="_blank" rel='noreferrer noopener' href={emojihack.personalWebsite}>tseeley.com</Link></p>
+                <p className=""><Link className="opacity-60 hover:opacity-100 group" target="_blank" rel='noreferrer noopener' href={emojihack.personalWebsite}><span className='group-hover:rotate-1'>✌️</span> tseeley.com</Link></p>
             </div>
         </footer>
     )
