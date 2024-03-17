b489e4d4361b3bdd8a08e7fe501d628ff6f96029
iamseeley <seeleypersonal@gmail.com>
2024-03-17 17:31:34 -0400

    trying to get png favicon to work on ios and safari

--- app/components/ProjectSpeed.tsx
+++ app/components/ProjectSpeed.tsx
@@ -11,28 +11,28 @@ interface ProjectSpeedProps {
     const rotationDegree = Math.min(mph, 200) / 200 * 180 - 90;
   
     return (
-      <div className="bg-black p-6 w-1/2 rounded-3xl shadow-md flex flex-col gap-4 items-center">
+      <div className="bg-black p-5 w-1/2 rounded-3xl shadow-md flex flex-col  gap-4 items-center">
         <div className="relative w-28 h-14 ">
           {/* Speedometer ring */}
           
             {/* Speedometer arc */}
             
-            <div className=" absolute rounded-t-full inset-0 bg-gradient-to-r from-green-400 via-yellow-300 to-red-600" />
-           
+            <div className="absolute rounded-t-full inset-0 bg-gradient-to-r from-green-400 via-yellow-300 to-red-600" />
+            <div className="absolute top-6 left-6 bg-black w-16 h-8 rounded-t-full"></div>
             {/* Needle */}
             <div 
-              className="w-0.5 h-16 bg-black absolute bottom-0 left-1/2 z-10 "
+              className="w-0.5 h-14 bg-white absolute bottom-0 ml-1 left-1/2 z-10 "
               style={{ transform: `rotate(${rotationDegree}deg)`, transformOrigin: 'bottom center'  }}
-            >
+            > 
               {/* Dot at the bottom of the needle */}
-              <div className="w-2.5 h-2.5 bg-black rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2"></div>
+              <div className="w-2.5 h-2.5 bg-white rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2"></div>
               {/* Point at the top of the needle */}
               {/* <div className="w-2 h-2 bg-black rotate-45 absolute top-0 left-1/2 transform -translate-x-1/2 -mt-1"></div> */}
             </div>
           </div>
           {/* Center Text */}
           <div className=" text-white">
-            <div className="text-xl font-bold text-center">{mph} MPH</div>
+            <div className="text-2xl font-bold text-center">{mph} MPH</div>
             <div className="text-sm font-medium text-gray-400 text-center">
               {projectsPerWeek} {projectsPerWeek === 1 ? "project" : "projects"}/week
             </div>
--- app/components/ProjectsStatus.tsx
+++ app/components/ProjectsStatus.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import emojihack from '../../emojihack.json'
+import Image from "next/image";
+
 
 interface ProjectStatusProps {
     totalEmojisCount: number;
@@ -14,15 +16,19 @@ interface ProjectStatusProps {
     progressPercentage = Math.max(progressPercentage, MIN_PROGRESS_PERCENTAGE);
   
     return (
-      <div className="bg-blue-400 p-6 rounded-3xl shadow-md w-1/2 flex flex-col justify-between">
+      <div className="bg-blue-400 p-5 rounded-3xl shadow-md w-1/2 flex flex-col justify-between">
+        <div className="flex flex-col gap-2">
+        <div><Image className="rounded-full" src={emojihack.profilepicPath} alt="avatar" height={44} width={44} /></div>
         <p className="font-semibold text-sm md:text-lg text-white">
           <Link href={'https://tseeley.com/about'} className="underline" target="_blank">{emojihack.preferredName}</Link> has launched {associatedEmojisCount} emoji projects out of {totalEmojisCount} emojis.
         </p>
+        
         <div className="bg-white  rounded-full h-3">
           <div 
             className="bg-green-400 rounded-l-full h-3" 
             style={{ width: `${progressPercentage}%` }}
           ></div>
+        </div>
         </div>
       </div>
     );
