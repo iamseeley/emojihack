e58629e94c0c341abcc7e3930ccc64ab21d691dd
iamseeley <seeleypersonal@gmail.com>
2024-03-18 13:56:27 -0400

    working on readme

--- setup.js
+++ setup.js
@@ -25,13 +25,33 @@ Your project content starts here...`;
 
 
 const homeContent = `import emojis from '../emojis/emojis.json'
+import { emojiArray } from '../emojis/emojis'
 import { getProjects } from "./utils/project";
 import ProjectStatus from "./components/ProjectsStatus";
+import Hero from "./components/Hero";
 import { ProjectSpeed } from "./components/ProjectSpeed";
-import { parseISO, differenceInCalendarWeeks, startOfWeek } from 'date-fns';
+import { parseISO, isWithinInterval } from 'date-fns';
 import ProjectsDisplay from "./components/ProjectsDisplay";
 
+import emojisOG from '../emojis/emojisOg.json';
+import type { Metadata } from 'next';
 
+
+const faviconUrl = \`data:image/svg+xml,\${encodeURIComponent(
+  \`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🛠️</text></svg>\`
+)}\`;
+
+const faviconPngData = emojisOG['🛠️'];
+
+
+
+export const metadata: Metadata = {
+  icons: { 
+    icon: faviconUrl,
+    shortcut: faviconPngData,
+    apple: faviconPngData,
+  },
+};
 
 
 
@@ -43,15 +63,39 @@ export default function Home() {
     return acc;
   }, {});
   // const associatedEmojis = Array.from(associatedEmojisSet);
-  const totalEmojisCount = emojis.length;
+  const totalEmojisCount = emojiArray.length;
   const associatedEmojisCount = Object.keys(emojiToProjectSlug).length;
-  const startDate = startOfWeek(parseISO('2024-03-04'), { weekStartsOn: 1 }); // weekStartsOn: 1 for Monday
-  const currentWeekStart = startOfWeek(new Date(), { weekStartsOn: 1 });
-  const totalWeeks = differenceInCalendarWeeks(currentWeekStart, startDate) + 1; // +1 to include the current week
-  const projectsPerWeek = associatedEmojisCount / totalWeeks;
+
+  const currentDate = new Date();
+  const oneWeekAgo = new Date(currentDate);
+  oneWeekAgo.setDate(currentDate.getDate() - 7);
+  const projectsInLastWeek = allProjects.filter(project =>
+    isWithinInterval(parseISO(project.metadata.publishedAt), {
+      start: oneWeekAgo,
+      end: currentDate,
+    })
+  );
+  const projectsPerWeek = projectsInLastWeek.length;
+
+  
+
 
   return (
     <>
+    <script
+        dangerouslySetInnerHTML={{
+          __html: \`
+            (function() {
+              var isMobileOrSafari = /Mobile|Safari/.test(navigator.userAgent);
+              var faviconElement = document.querySelector('link[rel="icon"]');
+              
+              if (isMobileOrSafari) {
+                faviconElement.href = '\${faviconPngData}';
+              }
+            })();
+          \`,
+        }}
+      />
       <div className="flex flex-col gap-20">
         <section className="flex flex-col gap-10">
           <div className="flex flex-row gap-4 mx-2">
@@ -63,8 +107,7 @@ export default function Home() {
       </div>
     </>
   );
-}
-`;
+}`;
 
 const homePagePath = path.join(__dirname, 'app', 'page.tsx');
 const heroComponentPath = path.join(__dirname, 'app', 'components', 'Hero.tsx');
